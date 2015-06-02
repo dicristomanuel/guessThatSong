6f72071c23942fc1d03a85516aeb1b604181bede
dicristomanuel <dicristomanuel@icloud.com>
2015-06-02 15:54:57 -0700

    first commit

--- songs.js
+++ songs.js
@@ -1,10 +1,74 @@
-$(function() {
 
-  var songIds = ["995535015", "966411602", "823593456", "956689796", "943946671",
-                 "982388023", "907242704", "201281527", "656801339", "910038357",
-                 "250038575", "878000348",  "794095205",  "1645339",  "400835962",
-                 "325618", "191924084",  "376116617",  "169003415",  "51958108",
-                 "76532142", "192688540", "684811768", "344799464", "217633921",
-                 "192811017", "258404365", "71068886", "640047583", "517438248" ];
-});
+  var songIds = [];
+  var current;
+  var count = 0;
 
+function randSong() {
+    if (songIds.length === 0) {
+        songIds = ["866276963", "611171954", "495911814", "266211028", "901445412",
+        "971648515", "904803519", "358822334", "502729174", "433399764", "844021161",
+        "701329988"];
+    }
+    var rand = parseInt(Math.random() * songIds.length);
+		var playThis = 'http://itunes.apple.com/lookup?id=' + songIds[rand]; 
+		current = playThis;   
+		songIds.splice(rand, 1);
+    return playThis;
+}
+ 
+
+function playSong() {
+	$("#preview").trigger("pause");
+	$.ajax({
+	   url: randSong(),
+	   jsonp: "callback",
+	   dataType: "jsonp"
+					}).done(function(response) {
+						var previewTune = response.results[0].previewUrl;
+						var previewField = $("#preview");
+	   		previewField.attr("src", previewTune).trigger("play");
+	});//ajax
+}//playSong
+
+				$("#play").click(function(evt) {
+					evt.preventDefault();
+					playSong();
+				})
+
+$("input").on('keypress', function(evt) {
+  var key = evt.which;
+    if (key === 13) {
+    	var userInput = $(this).val();
+     	$.ajax({
+	   url: current,
+	   jsonp: "callback",
+	   dataType: "jsonp"
+					}).done(function(response) {
+						userInput = userInput.toLowerCase();
+						artistName = response.results[0].artistName.toLowerCase();
+						trackName = response.results[0].trackName.toLowerCase().split(" (")[0];
+
+						if(userInput === artistName || userInput === trackName) {
+							count += 1;
+							$("#count").html(count);
+								var image = $("<img>").attr("src", response.results[0].artworkUrl100)
+								console.log(response.results[0].artworkUrl100);
+								$(".current").append(image);
+								setTimeout(function(){
+  								playSong();
+  							}, 2000);
+						} else {
+							setTimeout(function(){
+  								playSong();
+  							}, 2000);
+						}
+	});//ajax
+
+
+
+    }//ifStatement
+});//input.on'keypress'
+
+
+// https://itunes.apple.com/search?term=banks+goddess
+// http://itunes.apple.com/lookup?id="12312344"
